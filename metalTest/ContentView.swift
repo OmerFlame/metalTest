d6e458a95ba00be92946cc298a742139dc1811f7
Omer Shamai <renginald.bagel@gmail.com>
2020-09-01 21:21:57 +0300

    Initial Commit

---
+++ metalTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  metalTest
+//
+//  Created by Omer Shamai on 9/1/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ metalTest/metalTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  metalTestApp.swift
+//  metalTest
+//
+//  Created by Omer Shamai on 9/1/20.
+//
+
+import SwiftUI
+
+@main
+struct metalTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
